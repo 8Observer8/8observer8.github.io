57dcbda6a75eb0851211f2347921fb13994eea9b
Ivan Enzhaev <8observer8@gmail.com>
2019-07-31 17:30:49 +0400

    Added rotating

--- WebGL/a-lot-of-triangles_webgl10-typescript/shaders/vShader.glsl
+++ WebGL/a-lot-of-triangles_webgl10-typescript/shaders/vShader.glsl
@@ -1,9 +1,12 @@
 attribute vec2 aPosition;
 attribute vec3 aColor;
+
+uniform mat4 uModelMatrix;
+
 varying vec3 vColor;
             
 void main()
 {
-    gl_Position = vec4(aPosition, 0.0, 1.0);
+    gl_Position = uModelMatrix * vec4(aPosition, 0.0, 1.0);
     vColor = aColor;
 }
