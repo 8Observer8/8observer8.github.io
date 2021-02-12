15f6412a895e16aac8b0b6762d8a6009b097279d
Ivan Enzhaev <8observer8@gmail.com>
2021-02-13 02:10:06 +0400

    Delete debug files

--- webgl10-ts/pick-textured-object/js/RequireConfig.js
+++
@@ -1,8 +0,0 @@
-requirejs.config({
-    baseUrl: "js",
-    paths: {
-        "gl-matrix": "https://cdn.jsdelivr.net/npm/gl-matrix@3.3.0/gl-matrix-min"
-    }
-});
-requirejs(["main"], function () { });
-//# sourceMappingURL=RequireConfig.js.map
