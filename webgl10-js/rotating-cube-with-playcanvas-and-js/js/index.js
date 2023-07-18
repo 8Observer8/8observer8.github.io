91e63132b097aed00d007437a4e0d87c7056f13c
Ivan Enzhaev <8observer8@gmail.com>
2023-07-18 23:31:27 +0400

    Add the rotating-cube-with-playcanvas-and-js example

--- webgl10-js/rotating-cube-with-playcanvas-and-js/js/index.js
+++ webgl10-js/rotating-cube-with-playcanvas-and-js/js/index.js
@@ -1,1 +1,39 @@
-import{Application,FILLMODE_FILL_WINDOW,RESOLUTION_AUTO,Entity,Color}from"playcanvas-mjs";const canvas=document.getElementById("renderCanvas");const app=new Application(canvas);app.setCanvasFillMode(FILLMODE_FILL_WINDOW);app.setCanvasResolution(RESOLUTION_AUTO);window.addEventListener("resize",()=>app.resizeCanvas());const box=new Entity("cube");box.addComponent("model",{type:"box"});app.root.addChild(box);const camera=new Entity("camera");camera.addComponent("camera",{clearColor:new Color(.1,.1,.1)});app.root.addChild(camera);camera.setPosition(0,0,3);const light=new Entity("light");light.addComponent("light");app.root.addChild(light);light.setEulerAngles(45,0,0);app.on("update",dt=>box.rotate(10*dt,20*dt,30*dt));app.start();
+import { Application, Entity, Color } from 'playcanvas-mjs';
+
+const canvas = document.getElementById("renderCanvas");
+
+const app = new Application(canvas);
+
+// Fill the available space at full resolution
+// app.setCanvasFillMode(FILLMODE_FILL_WINDOW);
+// app.setCanvasResolution(RESOLUTION_AUTO);
+
+// Ensure canvas is resized when window changes size
+window.addEventListener("resize", () => app.resizeCanvas());
+
+// Create box entity
+const box = new Entity("cube");
+box.addComponent("model", {
+    type: "box"
+});
+app.root.addChild(box);
+
+// Create camera entity
+const camera = new Entity("camera");
+camera.addComponent("camera", {
+    clearColor: new Color(0.1, 0.1, 0.1)
+});
+app.root.addChild(camera);
+camera.setPosition(0, 0, 3);
+
+// Create directional light entity
+const light = new Entity("light");
+light.addComponent("light");
+app.root.addChild(light);
+light.setEulerAngles(45, 0, 0);
+
+// Rotate the box according to the delta time since the last frame
+app.on("update", dt => box.rotate(10 * dt, 20 * dt, 30 * dt));
+
+app.start();
+//# sourceMappingURL=index.js.map
