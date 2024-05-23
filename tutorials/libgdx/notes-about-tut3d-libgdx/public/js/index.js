bbeb0ed2a8ba2c1effccc7be586471ea9fe5abf1
Ivan Enzhaev <8observer8@gmail.com>
2024-05-23 18:37:14 +0400

    Add 'Notes about Tut3D'

---
+++ tutorials/libgdx/notes-about-tut3d-libgdx/public/js/index.js
@@ -0,0 +1,1 @@
+import"ejs";async function init(){const menuResponse=await fetch("/templates/menu.ejs");const menuTemplate=await menuResponse.text();document.getElementById("menu").innerHTML=menuTemplate}init();
