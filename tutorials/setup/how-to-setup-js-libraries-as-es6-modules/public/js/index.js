b93b5b0a6cd5379cf1da9aee2c0cda9bad234e30
Ivan Enzhaev <8observer8@gmail.com>
2024-09-28 22:07:36 +0400

    Updated

--- tutorials/setup/how-to-setup-js-libraries-as-es6-modules/public/js/index.js
+++
@@ -1,1 +0,0 @@
-import"ejs";async function init(){const menuResponse=await fetch("/templates/menu.ejs");const menuTemplate=await menuResponse.text();document.getElementById("menu").innerHTML=menuTemplate}init();
